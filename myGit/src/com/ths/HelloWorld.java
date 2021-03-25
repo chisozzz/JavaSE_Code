1d262f01d979f6f55c8ee95ae591d8edf2dca207
chisozzz <59664051+chisozzz@users.noreply.github.com>
2021-03-25 12:13:30 +0800

    第二次提交HelloWord

--- myGit/src/com/ths/HelloWorld.java
+++ myGit/src/com/ths/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("HelloWorld");
-
+        //我们只有从这开始才能完成这个任务
     }
 }
