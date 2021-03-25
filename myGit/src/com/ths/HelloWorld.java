205e1af54d6399c82d55e3166425c04caebd0eec
chisozzz <59664051+chisozzz@users.noreply.github.com>
2021-03-25 12:33:14 +0800

    master分支 第四次提交HelloWord

--- myGit/src/com/ths/HelloWorld.java
+++ myGit/src/com/ths/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        //我们只有从这开始才能完成这个任务
+
+        System.out.println("这是master输出的语句");
     }
 }
